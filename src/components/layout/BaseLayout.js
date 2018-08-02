7c30495e7ea3dda82c05ec8d1c64f709038e2f00
Peter Chen <peter.chen2@mail.mcgill.ca>
2018-08-02 15:30:47 +1000

    Separate out side drawer and top bar from base layout into its own component.

--- src/components/layout/BaseLayout.js
+++ src/components/layout/BaseLayout.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
-import Drawer from '@material-ui/core/Drawer'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import Hidden from '@material-ui/core/Hidden'
-import Divider from '@material-ui/core/Divider'
-import NavigationOptions from './NavigationOptions'
 import ContentContainer from './ContentContainer'
-import {Icon} from '@material-ui/core/es/index'
+import NavigationFrame from './NavigationFrame'
 
-const drawerWidth = 300
-
-const styles = theme => ({
+const styles = {
   root: {
     flexGrow: 1,
     height: 430,
@@ -23,94 +13,17 @@ const styles = theme => ({
     position: 'relative',
     display: 'flex',
     width: '100%'
-  },
-  appBar: {
-    position: 'absolute',
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${drawerWidth}px)`
-    }
-  },
-  navIconHide: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none'
-    }
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      position: 'relative'
-    }
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3
   }
-})
+}
 
 class BaseLayout extends React.Component {
-  state = {
-    mobileOpen: false
-  }
-
-  handleDrawerToggle = () => {
-    this.setState(state => ({mobileOpen: !state.mobileOpen}))
-  }
 
   render() {
-    const {classes, theme} = this.props
-
-    const drawer = (
-      <div>
-        <div className={classes.toolbar}/>
-        <Divider/>
-        <NavigationOptions/>
-      </div>
-    )
+    const {classes} = this.props
 
     return (
       <div className={classes.root}>
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.navIconHide}>
-              <Icon>menu</Icon>
-            </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
-              Trading Post
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Hidden mdUp>
-          <Drawer
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={this.state.mobileOpen}
-            onClose={this.handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper
-            }}
-            ModalProps={{
-              keepMounted: true // Better open performance on mobile.
-            }}>
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden smDown implementation="css">
-          <Drawer
-            variant="permanent"
-            open
-            classes={{
-              paper: classes.drawerPaper
-            }}>
-            {drawer}
-          </Drawer>
-        </Hidden>
+        <NavigationFrame/>
         <ContentContainer/>
       </div>
     )
---
+++ src/components/layout/NavigationFrame.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Hidden from '@material-ui/core/Hidden'
+import Divider from '@material-ui/core/Divider'
+import NavigationOptions from './NavigationOptions'
+import {Icon} from '@material-ui/core/es/index'
+
+const drawerWidth = 300
+
+const styles = theme => ({
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative'
+    }
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3
+  }
+})
+
+class NavigationFrame extends React.Component {
+  state = {
+    mobileOpen: false
+  }
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({mobileOpen: !state.mobileOpen}))
+  }
+
+  render() {
+    const {classes, theme} = this.props
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar}/>
+        <Divider/>
+        <NavigationOptions/>
+      </div>
+    )
+
+    return (
+      <React.Fragment>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}>
+              <Icon>menu</Icon>
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Trading Post
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}>
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper
+            }}>
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </React.Fragment>
+    )
+  }
+}
+
+NavigationFrame.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+}
+
+export default withStyles(styles, {withTheme: true})(NavigationFrame)
