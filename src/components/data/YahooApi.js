dc4bc9f9c9c24898f721764903e978dc53fc5ecf
Peter Chen <peter.chen2@mail.mcgill.ca>
2018-08-15 12:01:51 +1000

    Add method on YahooApi to get time series data.

--- src/components/data/YahooApi.js
+++ src/components/data/YahooApi.js
@@ -13,5 +13,45 @@ export default {
       exchange: data.meta.exchangeName,
       dailyClose: closingData ? closingData[ closingData.length - 1 ].toFixed(2) : 0
     }
+  },
+
+  getTimeSeriesData: async (symbol, chartInterval) => {
+    const interval = determineAppropriateDataInterval(chartInterval)
+    const endingPeriod = Math.round((new Date).getTime() / 1000)
+    const startingPeriod = endingPeriod - chartInterval
+    const response = await axios.get(
+      `https://cors.io/?https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?period1=${startingPeriod}&period2=${endingPeriod}&interval=${interval}`)
+    const data = response.data.chart.result[ 0 ]
+    
+    return {
+      symbol,
+      timeSeries: data.timestamp.map(unixtime => new Date(unixtime * 1000).toLocaleDateString("en-au")),
+      dataSeries: data.indicators.quote[ 0 ].close
+    }
   }
+}
+
+function determineAppropriateDataInterval(chartingPeriod) {
+  switch (chartingPeriod) {
+    case CHART.FIVE_YEARS:
+      return DATA_INTERVAL.MONTH
+    case CHART.YEAR:
+      return DATA_INTERVAL.WEEK
+    case CHART.MONTH:
+      return DATA_INTERVAL.DAY
+    default:
+      return DATA_INTERVAL.DAY
+  }
+}
+
+export const CHART = {
+  MONTH: 24 * 60 * 60 * 30,
+  YEAR: 24 * 60 * 60 * 365,
+  FIVE_YEARS: 24 * 60 * 60 * 365 * 5
+}
+
+const DATA_INTERVAL = {
+  DAY: '1d',
+  WEEK: '5d',
+  MONTH: '1mo'
 }
