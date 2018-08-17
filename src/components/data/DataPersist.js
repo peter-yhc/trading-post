60e25d0a27158351f57f2d566794dcf0edae6a3f
Peter Chen <peter.chen2@mail.mcgill.ca>
2018-08-17 14:15:44 +1000

    Remove unused function.

--- src/components/data/DataPersist.js
+++ src/components/data/DataPersist.js
@@ -12,16 +12,3 @@ export function updateStocks(symbol, updatedValues) {
 export function retrieveStocks() {
   return JSON.parse(localStorage.getItem('stocks')) || []
 }
-
-export function retrieveStock(symbol) {
-  // let cache = JSON.parse(localStorage.getItem('stocks')) || []
-  // if (cache.length > 0) {
-  //   const stock = cache.find(stock => stock.symbol === symbol)
-  //   if (moment().diff(stock.fetchedAt, 'days') === 0) {
-  //     console.log(`Cache is valid: ${symbol}`)
-  //     return stock
-  //   }
-  // }
-
-  return retrieveStocks().find(stock => stock.name === symbol)
-}
